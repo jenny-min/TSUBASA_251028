9a405f89ee94d23cc5dc29aae8b189e4c75ffa83
jenny-min <jennynguyen.92.92@gmail.com>
2025-12-08 17:09:59 +0900

    create exercise01 folder

---
+++ Module01/session14/exercise01/exercise01.js
@@ -0,0 +1,11 @@
+//Tạo 1 đối tượng là person
+let person = {
+    name: 'John Doe',
+    age: 25,
+    job: 'Developer'
+}
+
+//Sử dụng console để in ra thông tin đối tượng
+console.log(`Họ và tên: ${person.name}`);
+console.log(`Tuổi: ${person.age} tuổi`);
+console.log(`Công việc: ${person.job}`);
