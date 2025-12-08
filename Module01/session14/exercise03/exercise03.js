eec052176f09d99e73722327bf560e3f85983f90
jenny-min <jennynguyen.92.92@gmail.com>
2025-12-08 17:24:52 +0900

    create exercise03 folder

---
+++ Module01/session14/exercise03/exercise03.js
@@ -0,0 +1,12 @@
+// Tạo đối tượng nook
+let book = {
+    title: 'JavaScript Basics',
+    author: 'John Smith',
+    page: 200
+}
+
+//Xóa thuộc tính
+delete book.page;
+
+// In ra đối tượng
+console.log(book);
