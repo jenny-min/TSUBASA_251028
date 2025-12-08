fd8aeff5a8b5d26517bb0053557b0184198c1c20
jenny-min <jennynguyen.92.92@gmail.com>
2025-12-08 17:18:48 +0900

    create exercise02 folder

---
+++ Module01/session14/exercise02/exercise02.js
@@ -0,0 +1,15 @@
+//Tạo đối car
+let car = {
+    brand: 'Toyota',
+    model: 'Corolla',
+    year: 2020
+}
+
+//Thêm thuộc tính color: red vào car
+car.color = 'red';
+
+// Cập nhật giá trị
+car.year = 2022;
+
+// In ra đối tượng
+console.log(car);
