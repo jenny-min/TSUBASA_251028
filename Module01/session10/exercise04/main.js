14e0e67502d01c1d319b727e18df478f2bd568df
jenny-min <jennynguyen.92.92@gmail.com>
2025-12-10 23:04:46 +0900

    create exercise04 folder

---
+++ Module01/session10/exercise04/main.js
@@ -0,0 +1,3 @@
+let firstNumber = Number(prompt("Nhập vào số thứ nhất:"));
+let secondNumber = Number(prompt("Nhập vào số thứ hai:"));
+console.log("Tổng của hai số là: ", + (firstNumber + secondNumber));
