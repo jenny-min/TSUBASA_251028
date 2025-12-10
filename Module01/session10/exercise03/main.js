1d3047e0cc8b133baf10fd09290c8ae0129b2b18
jenny-min <jennynguyen.92.92@gmail.com>
2025-12-10 22:57:29 +0900

    create exercise03 folder

---
+++ Module01/session10/exercise03/main.js
@@ -0,0 +1,3 @@
+let width = Number(prompt("Nhập vào chiều rộng của hình chữ nhật:"));
+let height = Number(prompt("Nhập vào chiều cao của hình chữ nhật:"));
+console.log("Diện tích của hình chữ nhật là: " + width * height);
