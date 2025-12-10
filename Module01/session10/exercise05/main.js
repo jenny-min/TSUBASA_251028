416fcdd83e68f398aca7cb102be59319942e73cf
jenny-min <jennynguyen.92.92@gmail.com>
2025-12-10 23:13:39 +0900

    create exercise05 folder

---
+++ Module01/session10/exercise05/main.js
@@ -0,0 +1,3 @@
+const usd = Number(prompt('Vui lòng nhập số tiền cần chuyển đổi (đơn vị: USD):'));
+const rate = +25000;
+console.log('Số tiền chuyển đổi là: ' + (usd * rate) + ' Việt Nam Đồng (VND)');
