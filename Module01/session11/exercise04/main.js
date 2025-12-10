94ce725b912f8f47c359ea2da08be6926c2b50ec
jenny-min <jennynguyen.92.92@gmail.com>
2025-12-10 23:43:30 +0900

    create exercise04 folder

---
+++ Module01/session11/exercise04/main.js
@@ -0,0 +1,5 @@
+//Viết chương trình hiển thị chuỗi “The number is N” 5 lần sử dụng vòng lặp for. Với N sẽ hiển thị từ 0 đến 5.
+for (let i = 0; i <= 5; i++) {
+    console.log(`The number is ${i}`);
+
+}
