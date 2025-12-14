aa437f7b52b0955f7cbc45f14b3c84c34f4bc322
jenny-min <jennynguyen.92.92@gmail.com>
2025-12-14 22:20:07 +0900

    update

--- Module01/session17/exercise01/main.js
+++ Module01/session17/exercise01/main.js
@@ -1,7 +1,8 @@
 let text = document.querySelector('.text');
 let btnHide = document.querySelector('.hide');
 let btnShow = document.querySelector('.show');
-
+//ẩn text bằng onclick vào button thì function bên dưới sé được thực hiện
+//thêm style {display: none} cho p
 btnHide.onclick = function () {
     text.style.display = 'none';
 };
