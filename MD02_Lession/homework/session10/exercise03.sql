ceabc15bcaa8e627373467b37821e382babb4729
jenny-min <jennynguyen.92.92@gmail.com>
2026-02-07 11:16:47 +0900

    add session10/exercise03.sql file

---
+++ MD02_Lession/homework/session10/exercise03.sql
@@ -0,0 +1,21 @@
+-- [Bài tập] Tự động kiểm tra số lượng sản phẩm trước khi insert
+-- Tạo trigger
+DELIMITER $$
+
+CREATE TRIGGER BeforeInsertProduct
+BEFORE INSERT ON Products
+FOR EACH ROW
+BEGIN
+    IF NEW.quantity < 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Số lượng sản phẩm không được nhỏ hơn 0!';
+    END IF;
+END $$
+
+DELIMITER ;
+ 
+-- Chèn sản phẩm hợp lệ
+INSERT INTO Products (product_name, quantity) VALUES ('Sản phẩm hợp lệ', 10);
+
+-- Chèn sản phẩm với quantity < 0 -> sẽ bị lỗi và không chèn được
+INSERT INTO Products (product_name, quantity) VALUES ('Sản phẩm lỗi', -5);
